11abd2faf8dbfe2ae25372a7aa9c59f99e280118
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2018-03-04 21:49:56 +0300

    no message

--- TableContainer/Row.swift
+++ TableContainer/Row.swift
@@ -12,12 +12,12 @@ public struct Row : IContainerItem
     public let id:String;
     public let sortKey:String?;
     public var model:AnyObject?;
-    public var cell:Cell?
+    public var cell:Cell
     
     public init(id:String = "",
                 sortKey:String? = nil,
                 model:AnyObject? = nil,
-                cell:Cell?)
+                cell:Cell)
     {
         self.id = id;
         self.sortKey = sortKey;
