08b899a6039a8bdf34ac74ce18efebba77fda494
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2018-03-04 21:41:41 +0300

    - added cell

---
+++ TableContainer/Cell.swift
@@ -0,0 +1,17 @@
+//
+//  Cell.swift
+//  TableContainer
+//
+//  Created by Alexey Ivankov on 04.03.2018.
+//  Copyright © 2018 Alexey Ivankov. All rights reserved.
+//
+
+import Foundation
+
+public struct Cell {
+    
+    public var reuseId:String?
+    public var nibName:String?
+    public var loadClass:AnyClass?
+    
+}
--- TableContainer/Row.swift
+++ TableContainer/Row.swift
@@ -12,13 +12,16 @@ public struct Row : IContainerItem
     public let id:String;
     public let sortKey:String?;
     public var model:AnyObject?;
+    public var cell:Cell?
     
     public init(id:String = "",
                 sortKey:String? = nil,
-                model:AnyObject? = nil)
+                model:AnyObject? = nil,
+                cell:Cell?)
     {
         self.id = id;
         self.sortKey = sortKey;
         self.model = model;
+        self.cell = cell
     }
 }
